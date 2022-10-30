c060cb48953780fcb8e308b6bf05d419a2734fbe
MaalawiMahdi <mehdi.maalawi@esprit.tn>
2022-10-30 17:06:26 +0100

    fix StockServiceMockTest

--- src/test/java/tn/esprit/rh/achat/StockServiceMockTest.java
+++ src/test/java/tn/esprit/rh/achat/StockServiceMockTest.java
@@ -8,15 +8,19 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.entities.StockRequestModel;
 import tn.esprit.rh.achat.repositories.StockRepository;
 import tn.esprit.rh.achat.services.StockServiceImpl;
-
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class StockServiceMockTest {
 	
 	@Mock
@@ -24,12 +28,9 @@ class StockServiceMockTest {
 	@InjectMocks
 	StockServiceImpl StockServiceImpl;
 	StockRequestModel s= new StockRequestModel("libelle1",100,50);
-	
 	Stock s2= new Stock("libelle2",150,0);
 	Stock s3= new Stock("libelle3",130,70);
 	Stock s4= new Stock("libelle4",100,50);
-
-	
 	List<Stock> list = new ArrayList<Stock>() {
 		{	
 			add(s2);
