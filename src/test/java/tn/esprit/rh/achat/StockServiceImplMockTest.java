e537423cb8c9ba047b92e2908c2a78531fd98c46
MaalawiMahdi <mehdi.maalawi@esprit.tn>
2022-10-30 16:53:06 +0100

    fix StockServiceImpMockTest

--- src/test/java/tn/esprit/rh/achat/StockServiceImplMockTest.java
+++ src/test/java/tn/esprit/rh/achat/StockServiceImplMockTest.java
@@ -8,15 +8,19 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.entities.StockRequestModel;
 import tn.esprit.rh.achat.repositories.StockRepository;
 import tn.esprit.rh.achat.services.StockServiceImpl;
-
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class StockServiceImplMockTest {
 	
 	@Mock
